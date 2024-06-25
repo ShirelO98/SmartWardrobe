7b262bb93c4f7b8b6c9f8abaf307e51577dfcbf8
AdarAfek <adar257@gmail.com>
2024-06-25 19:29:44 +0300

    db connection prpogress
    add the initialize func to the index

--- server/db.js
+++ server/db.js
@@ -1,27 +1,34 @@
-exports.db = {
+const mysql = require("mysql2/promise");
+let connection;
+module.exports = {
   async createConnection() {
-    const mysql = require("mysql2/promise");
-    const connection = await mysql.createConnection({
+    connection = await mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
     });
   },
-  async  initialize(){
-  await connection.execute(`CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-`
-);
-await connection.execute(`CREATE TABLE wardrobes_1 (
-    wardrobe_id INT AUTO_INCREMENT PRIMARY KEY,
-    wardrobe_name VARCHAR(100) NOT NULL
-);
-`)
-  }
+  async initialize() {
+    if (connection) {
+      await connection.execute(`CREATE TABLE users (
+            user_id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50) NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        `);
+      await connection.execute(`CREATE TABLE wardrobes_1 (
+            wardrobe_id INT AUTO_INCREMENT PRIMARY KEY,
+            wardrobe_name VARCHAR(100) NOT NULL
+        );
+        `);
+    }
+  },
+  async closeConnection() {
+    if (connection) {
+      await connection.end();
+    }
+  },
 };
